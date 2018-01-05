682ff2109c6fbe76bf0f69d985767d992adf9f8b
Clara Ameller <claraameller@Claras-MacBook-Pro.local>
2018-01-05 12:30:17 +0100

    Done

--- src/components/Title.js
+++ src/components/Title.js
@@ -9,26 +9,26 @@ class Title extends React.Component {
     this.renameBand = this.renameBand.bind(this);
   }
 
-    // Name band handler
-    renameBand(event) {
-      if (event.target.value.length === 0) {
-        this.setState({title: 'The Perfect Band'});
-      } else {
-        this.setState({title: event.target.value});
-      }
+  // Name band handler
+  renameBand(event) {
+    if (event.target.value.length === 0) {
+      this.setState({title: 'The Perfect Band'});
+    } else {
+      this.setState({title: event.target.value});
     }
+  }
 
-    render() {
-      let input;
-      return (
-        <div>
-          <h1 className='text-center'>{this.state.title}</h1>
-          <form className='form my-3'>
-            <input autoFocus type='text' className='form-control' placeholder='Your band name' onChange={this.renameBand} />
-          </form>
-        </div>
-      );
-    }
+  render() {
+    let input;
+    return (
+      <div>
+        <h1 className='text-center'>{this.state.title}</h1>
+        <form className='form my-3'>
+          <input autoFocus type='text' className='form-control' placeholder='Your band name' onChange={this.renameBand} />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Title;
